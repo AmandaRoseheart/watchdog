9599a2eb6cc084e8848214c7e9c22bc209de29df
AmandaRoseheart <amanda.roseheart@protonmail.com>
2021-03-03 09:51:53 +0100

    adds sending of mails; adds deletion of monitored urls when they have changed

--- src/main/java/org/amandaroseheart/watchdog/tasks/CheckPages.java
+++ src/main/java/org/amandaroseheart/watchdog/tasks/CheckPages.java
@@ -1,7 +1,10 @@
 package org.amandaroseheart.watchdog.tasks;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.amandaroseheart.watchdog.domain.WebPage;
 import org.amandaroseheart.watchdog.repositories.WebPageRepo;
+import org.amandaroseheart.watchdog.services.MailService;
 import org.amandaroseheart.watchdog.services.WatchdogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,20 +17,33 @@ public class CheckPages {
   WebPageRepo repo;
 
   @Autowired
-  WatchdogService service;
+  WatchdogService watchdogService;
+
+  @Autowired
+  MailService mailService;
 
   @Scheduled(cron = "0 0 3 * * ?")
   public void execute() {
     System.out.println("Task started");
+    List<String> toBeDeleted = new ArrayList<>();
     for (WebPage webPage : repo.getAll()) {
       String md5then = webPage.getMd5();
       String md5now = null;
       try {
-        md5now = service.calculateMd5(webPage.getUrl(), webPage.getXpath());
+        md5now = watchdogService
+            .calculateMd5(webPage.getUrl(), webPage.getXpath());
+        if (!md5then.equals(md5now)) {
+          final String message = String
+              .format("Watchdog detected that %s has changed!",
+                  webPage.getUrl());
+          mailService.sendMail(message);
+          toBeDeleted.add(webPage.getMd5());
+        }
       } catch (Exception e) {
         e.printStackTrace();
       }
-      if (!md5then.equals(md5now)) System.out.println("Send mail");
+
     }
+    toBeDeleted.stream().forEach(md5 -> repo.delete(md5));
   }
 }
